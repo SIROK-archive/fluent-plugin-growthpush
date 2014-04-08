e7647519c8657a065e81ce6b70a876ab655824b7
sota0805 <sota.yamashita@gmail.com>
2014-04-08 23:15:34 +0900

    First commit

---
+++ lib/fluent/plugin/growthpush.rb
@@ -0,0 +1,9 @@
+require "fluent/plugin/growthpush/version"
+
+module Fluent
+  module Plugin
+    module Growthpush
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/fluent/plugin/growthpush_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Fluent::Plugin::Growthpush do
+  it 'should have a version number' do
+    Fluent::Plugin::Growthpush::VERSION.should_not be_nil
+  end
+
+  it 'should do something useful' do
+    false.should be_true
+  end
+end
