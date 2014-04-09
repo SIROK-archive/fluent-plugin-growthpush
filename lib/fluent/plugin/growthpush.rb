459c912b1255ec67f4047f6f7890be75f65d1348
sota0805 <sota.yamashita@gmail.com>
2014-04-09 10:45:19 +0900

    Add: growthpush.rb: stdout record

--- lib/fluent/plugin/growthpush.rb
+++ lib/fluent/plugin/growthpush.rb
@@ -7,4 +7,26 @@ require "json"
 class Fluent::GrowthPushOutput < Fluent::Output
   Fluent::Plugin.register_output('growthpush', self)
 
+  config_param :application_id, :string
+  config_param :secret, :string
+  
+  def initialize 
+    super
+    @growthpush = GrowthPush.new(@application_id, @secret)
+  end
+
+  def config(conf)
+  	super
+  end
+
+
+  def emit(tag, es, chain)
+    chain.next
+    es.each {|time,record|
+      $stderr.puts "#{record}"
+      # @growthpush.create_event(token, name, value);
+      # p GrowthPush.new(@application_id, @secret)
+    }
+  end
+
 end
