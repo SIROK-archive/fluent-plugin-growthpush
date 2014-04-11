fe6b8dd64ba3c77b4edbbb9bbebf6581ae36806b
sota0805 <sota.yamashita@gmail.com>
2014-04-11 10:52:58 +0900

    Debug: out_growthpush.rb:

--- lib/fluent/plugin/out_growthpush.rb
+++ lib/fluent/plugin/out_growthpush.rb
@@ -22,14 +22,15 @@ class Fluent::GrowthPushOutput < Fluent::Output
   def emit(tag, es, chain)
     chain.next
     es.each do |time,record|
-      case record["api"] 
-      when "client" then 
-        @growthpush.create_event(record["token"], record["os"])
-      when "event" then 
-        @growthpush.create_event(record["client"], record["name"], record["value"])
-      when "tag" then 
-        @growthpush.create_event(record["client"], record["name"], record["value"])
-      end
+      # case record["api"] 
+      # when "client" then 
+      #   @growthpush.create_client(record["token"], record["os"])
+      # when "event" then 
+      #   @growthpush.create_event(record["client"], record["name"], record["value"])
+      # when "tag" then 
+      #   @growthpush.create_tag(record["client"], record["name"], record["value"])
+      # end
+      p record["api"]
     end
   end
 
