7bce4eb49c0eb6ea8f5dce1916af907383f18749
sota0805 <sota.yamashita@gmail.com>
2014-04-11 12:15:10 +0900

    Refactoring: out_growthpush.rb

--- lib/fluent/plugin/out_growthpush.rb
+++ lib/fluent/plugin/out_growthpush.rb
@@ -1,8 +1,3 @@
-require "growth_push"
-require "json"
-require "pry"
-require "awesome_print"
-
 class Fluent::GrowthPushOutput < Fluent::Output
   Fluent::Plugin.register_output('growthpush', self)
 
@@ -11,28 +6,29 @@ class Fluent::GrowthPushOutput < Fluent::Output
   
   def initialize
     super
-    growthpush = GrowthPush.new(@application_id, @secret)
+    require "growth_push"
   end
 
-  def config(conf)
-  	super
-  end
 
   def emit(tag, es, chain)
     chain.next
     es.each do |time,record|
-      # GrowthPush.new(@application_id, @secret)
-      case record["api"]
-      when "client"
-        growthpush.create_client(record["token"], record["os"])
-      when "event"
-        growthpush.create_event(record["client"], record["name"], record["value"]=nil)
-      when "tag"
-        growthpush.create_tag(record["client"], record["name"], record["value"]=nil)
-      end
+      call(record)
     end
   end
 
+
+  def call(record)
+    growthpush = GrowthPush.new(@application_id, @secret)
+    case record["api"]
+    when "client"
+      growthpush.create_client(record["token"], record["os"])
+    when "event"
+      growthpush.create_event(record["client"], record["name"], record["value"]=nil)
+    when "tag"
+      growthpush.create_tag(record["client"], record["name"], record["value"]=nil)
+    end
+  end
 
 
 end
